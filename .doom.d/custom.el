1b6a6a01bd62ecb951656df18429ef1683cfac47
Simon Thomson <simon@Simon-Laptop>
2022-07-24 20:10:54 +0100

    commit

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -3,10 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(lsp-mode pmdm org-roam-ui treeview sqlite3)))
+ '(org-agenda-files '("~/roam/holiday-todo.org"))
+ '(package-selected-packages
+   '(deft markdownfmt dap-mode cmake-mode yasnippet yaml which-key vterm treeview treemacs-tab-bar treemacs-projectile treemacs-magit treemacs-icons-dired treemacs-evil treemacs-all-the-icons terminal-toggle terminal-here term-projectile sqlite3 pmdm org-roam-ui lsp-mode helm-xref flycheck company-glsl bui)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#282c34" :foreground "#bbc2cf" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "PfEd" :family "DejaVu Sans Mono")))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#323f4e" :foreground "#f8f8f2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 180 :width normal :foundry "PfEd" :family "DejaVu Sans Mono")))))
+(put 'customize-face 'disabled nil)
